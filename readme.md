e6477c0a3391f278456ae7d3b16b9bb398253a6f
Nic Cottrell <nic@cottrell.net>
2018-06-21 10:24:37 +0200

    Added examples of schema migration (adding and transforming fields) Expanded readme

--- readme.md
+++ readme.md
@@ -1,8 +1,29 @@
 # Mongobee examples
 
-This is intended to show some working examples of [mongoBee](https://github.com/mongobee/mongobee)
+This is intended to show some working examples of [mongoBee](https://github.com/mongobee/mongobee) - a Java tool which helps you to manage changes in your MongoDB and synchronize them with your application. The concept is very similar to other db migration tools such as Liquibase or Flyway but without using XML/JSON/YML files.
 
 Note that indexes are built in the background to avoid locking the database in production.
+
+## Changeset
+ 
+The example ChangeLog shows:
+ * Creating different types of indexes
+ * Creating 'core' documents from code
+ * Cloning 'core' collections from a template database (not version controlled)
+ * Importing documents from JSON content under version control
+ * Migrating to a new schema by adding new fields or transforming existing fields to a new data type
+
+### addDefaultField
+
+Changes schema design with new default field. It uses two calls of updateMany to update documents in place on the server with minimal data transfer 
+
+Note: runAlways = true since the filter itself excludes any documents that have already been migrated
+     
+### changeFieldType
+     
+Migrates the points field to a decimal version (with a different field name) as per [best practices](https://docs.mongodb.com/manual/tutorial/model-monetary-data/).
+         
+Note: Order is 402 to guarantee it is run after addDefaultField above
 
 # Build
 
--- src/main/java/niccottrell/migration/DatabaseChangelog.java
+++ src/main/java/niccottrell/migration/DatabaseChangelog.java
@@ -5,13 +5,16 @@ import com.github.mongobee.changeset.ChangeSet;
 import com.mongodb.Block;
 import com.mongodb.MongoWriteException;
 import com.mongodb.client.*;
-import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.*;
 import org.apache.commons.io.FileUtils;
+import org.bson.BsonType;
 import org.bson.Document;
+import org.bson.types.Decimal128;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -69,6 +72,85 @@ public class DatabaseChangelog {
                 new IndexOptions()
                         .expireAfter(0L, TimeUnit.SECONDS)
                         .background(true));
+    }
+
+    /**
+     * Change schema design with new default field.
+     * Note: runAlways = true since the filter itself excludes any documents that have already been migrated
+     */
+    @ChangeSet(order = "401", id = "addDefaultField", author = "nic", runAlways = true)
+    public void addDefaultField(MongoDatabase db) {
+        // Add new "freqTravelPoints" field and give "Gold" members 100 points, otherwise 0
+        // Change from:
+        // { name: "John Smith",
+        //   status: "Gold"
+        //    ... }
+        //  to
+        // { name: "John Smith",
+        //   status: "Gold",
+        //   freqTravelPoints: 100
+        //    ... }
+        MongoCollection<Document> collection = db.getCollection("customers");
+        // We want to avoid loading any documents over the network (imagine we have 1M customers!)
+        // Update all Gold customers first with 100 points
+        collection.updateMany(Filters.and(
+                Filters.exists("freqTravelPoints", false),
+                Filters.eq("status", "Gold")
+        ), Updates.set("freqTravelPoints", 100));
+        // Update all other customers with 0 points
+        collection.updateMany(Filters.and(
+                Filters.exists("freqTravelPoints", false)
+        ), Updates.set("freqTravelPoints", 0));
+        // Now all customers should have the freqTravelPoints field (except for newly inserted documents)
+    }
+
+    /**
+     * Migrate the points field to a decimal version (with a different field name)
+     * See: https://docs.mongodb.com/manual/tutorial/model-monetary-data/
+     * <p>
+     * Note: Order is 402 to guarantee it is run after addDefaultField above
+     */
+    @ChangeSet(order = "402", id = "changeFieldType", author = "nic", runAlways = true)
+    public void changeFieldType(MongoDatabase db) {
+        // The new "travelCredits" field is 1/10 of the original freqTravelPoints value
+        // Change from:
+        // { name: "David Smith",
+        //   status: "Gold"
+        //   freqTravelPoints: 150
+        //    ... }
+        //  to
+        // { name: "David Smith",
+        //   status: "Gold",
+        //   freqTravelPoints: 150
+        //   travelCredits: 15.0
+        //    ... }
+        MongoCollection<Document> collection = db.getCollection("customers");
+        // Unlike in addDefaultField(), we need to load and save documents in the client (i.e. network transfers)
+        AggregateIterable<Document> iterable = collection.aggregate(Arrays.asList(
+                // Only look for customers who do not already have a travelCredits field
+                Aggregates.match(
+                        Filters.and(
+                                Filters.type("freqTravelPoints", BsonType.INT32),
+                                // only if the field does not already exist
+                                Filters.exists("travelCredits", false)
+                        )
+                ),
+                // Only consider the freqTravelPoints field
+                Aggregates.project(new Document("freqTravelPoints", 1)),
+                // Now caclulate the new value directly in MongoDB (we could also do it in code)
+                Aggregates.addFields(new Field<>("travelCredits",
+                        new Document("$multiply", Arrays.asList("$freqTravelPoints", Decimal128.parse("0.1"))))
+                )
+        ));
+        for (Document calculated : iterable) {
+            // we'll use findOneAndUpdate rather than replaceOne because:
+            //   1. we avoid sending the entire customer document over the wire (could be large)
+            //   2. we only change the new field (simultaneous changes from other threads will not be overwritten)
+            collection.findOneAndUpdate(
+                    Filters.eq(calculated.get("_id")),
+                    Updates.set("travelCredits", calculated.get("travelCredits")));
+        }
+        // Now all customers should have the travelCredits field as a Decimale
     }
 
     @ChangeSet(order = "600", id = "loadSchedules", author = "nic")
